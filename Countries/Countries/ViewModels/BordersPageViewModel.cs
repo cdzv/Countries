c17047cc19e5fa64158c3c78f5251856ad65afc8
cdzv <cristian.danilo222@gmail.com>
2019-10-22 09:37:45 -0500

    Pre - Show maps

--- Countries/Countries/ViewModels/BordersPageViewModel.cs
+++ Countries/Countries/ViewModels/BordersPageViewModel.cs
@@ -1,10 +1,7 @@
 ﻿using Countries.Helpers;
 using Countries.Models;
 using Newtonsoft.Json;
-using Prism.Commands;
-using Prism.Mvvm;
 using Prism.Navigation;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
