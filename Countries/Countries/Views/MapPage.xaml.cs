45858cf5540a9d32d5b87014c865bfa6d995c992
cdzv <cristian.danilo222@gmail.com>
2019-10-23 12:26:17 -0500

    Distance

--- Countries/Countries/Views/MapPage.xaml.cs
+++ Countries/Countries/Views/MapPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Countries.Helpers;
+﻿using System;
+using Countries.Helpers;
 using Countries.Models;
 using Newtonsoft.Json;
 using Xamarin.Forms;
@@ -8,13 +9,35 @@ namespace Countries.Views
 {
     public partial class MapPage : ContentPage
     {
+        private Country _country;
+
         public MapPage()
         {
             InitializeComponent();
-            var _country = JsonConvert.DeserializeObject<Country>(Settings.Country);
+            _country = JsonConvert.DeserializeObject<Country>(Settings.Country);
+            MoveMapToCountry();
+        }
+
+        private void MoveMapToCountry()
+        {
+            var _latitude = _country.Latlng[0];
+            var _longitude = _country.Latlng[1];
+
+            var _distance = Convert.ToInt32((_country.Area) * 0.001);
+
+            if (_distance < 100) { _distance = 50; }
+
+
             MapView.MoveToRegion(
                 MapSpan.FromCenterAndRadius(
-                    new Position(_country.Latlng[0], _country.Latlng[1]), Distance.FromKilometers(800)));
+                    new Position(_latitude, _longitude), Distance.FromKilometers(_distance)));
+            MapView.Pins.Add(
+                new Pin
+                {
+                    Label = _country.Name,
+                    Position = new Position(_latitude, _longitude),
+                    Type = PinType.Place
+                });
         }
     }
 }
